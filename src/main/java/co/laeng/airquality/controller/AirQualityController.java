6be6d468b568596668c09e83571d44c1bfd97c67
Laeng <Laeng@users.noreply.github.com>
2023-01-05 21:45:20 +0900

    chore: 사용하지 않는 import 문 제거

--- src/main/java/co/laeng/airquality/controller/AirQualityController.java
+++ src/main/java/co/laeng/airquality/controller/AirQualityController.java
@@ -6,9 +6,6 @@ import co.laeng.airquality.service.AirQualityService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
